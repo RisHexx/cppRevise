f09e3e1da37b8a483f0fb0995ce26563a3a16a73
RisHexx <rishabhkanojiya9082@gmail.com>
2025-07-28 19:39:33 +0530

    Done

---
+++ Pointers/nullPointer.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    // Null Pointer is pointer holding null values
+    // while declaring pointer if we dont know where he is going to point assign it to null will be 
+    // best practice as we dont want to it point randomly
+
+    int *ptr = nullptr;
+    cout<<ptr<<endl;
+    cout<<*ptr<<endl;
+    
+    return 0;
+}
